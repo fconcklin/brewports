a884a2fcc8ed468c35814cd14b2338064dc38ca3
fred concklin <fredconcklin@gmail.com>
2013-12-28 22:20:51 +0100

    wrapped with checks

--- brewports.sh
+++ brewports.sh
@@ -3,11 +3,55 @@
 # Update installed package managers 
 
 # todo: check for brew, only update if installed
-brew update 
-brew install `brew outdated`
 
-# todo: check for macports, only update if installed 
-sudo port selfupdate 
+installed()
+{
+    command -v $1 >/dev/null 2>&1
+}
 
-# todo: check for rvm, only update if installed
-rvm get head 
+check_homebrew()
+{
+    echo "checking for homebrew"
+    if [ $installed "brew" ]; then
+	echo "found homebrew"
+	echo "updating brew"
+	brew update 
+	echo "upgrading brew packages"
+	brew install `brew outdated`
+    else 
+	echo "homebrew not found. skipping"
+    fi 
+}
+
+check_macports()
+{
+    echo "checking for macports"
+    if [ $installed "port" ]; then 
+	echo "macports found"
+	echo "updating macports"
+	sudo port selfupdate 
+    else
+	echo "macports not found. skipping."
+    fi
+}
+
+check_rvm()
+{
+    echo "checking for rvm"
+    if [ $installed "rvm" ]; then
+	echo "rvm found" 
+	echo "updating rvm"
+	rvm get head 
+    else 
+	echo "rvm not found. skipping."
+    fi
+}
+
+run()
+{
+    check_homebrew
+    check_macports
+    check_rvm
+}
+
+run 
